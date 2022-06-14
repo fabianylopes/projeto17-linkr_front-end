ae8d1319d9829377ad0d1277ed42d7b517bfd429
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-06-14 15:20:02 -0300

    docs: update dependences; Icon index.html

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import '../css/reset.css';
+import '../css/style.css';
+
 function App(){
 
     return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import { render } from 'react-dom';
 
-import './css/reset.css';
-import './css/style.css';
-import App from './App';
+import App from './components/App.js';
 
 render(<App/>, document.querySelector('.root'));
